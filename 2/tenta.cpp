6d0df28dd7792accb6ac22547d91f55fe86d6cd7
Arthur Câmara <camara.arthur@gmail.com>
2015-03-30 20:30:52 -0300

    made TENTA faster

--- 2/tenta.cpp
+++ 2/tenta.cpp
@@ -2,25 +2,43 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-
+#include <sstream>
 using namespace std;
+
+
+vector<unsigned> parseLine(string line){
+  vector<unsigned> ret;
+  unsigned n = 0;
+  stringstream stream(line);
+  while(stream >> n)
+    ret.push_back(n);
+  return ret;
+}
+
+
+
 int main(int argc, char *argv[]) {
-  unsigned numberOfElements;
-  cin >> numberOfElements;
-  while(numberOfElements!=0){
-    vector<unsigned> itens;
-    unsigned item;
-    for(unsigned in = 0; in <numberOfElements; in++){
-      cin>>item;
-      itens.push_back(item);
-    }
+  string in;
+  getline(std::cin, in);
+  vector<unsigned> itens = parseLine(in);
+  unsigned numberOfElements = itens[0];
+  itens.erase(itens.begin());
+  bool counter = true;
+
+  while(1){
     sort(itens.begin(), itens.end());
-    do{
-      for (unsigned counter = 0; counter<numberOfElements; counter++)
+    do {
+      for(unsigned counter = 0; counter<numberOfElements; counter++)
         cout<<itens[counter]<<" ";
       cout<<endl;
-    }while (next_permutation(itens.begin(), itens.end()));
+    } while(next_permutation(itens.begin(), itens.end()));
+
+    getline(std::cin, in);
+    itens = parseLine(in);
+    numberOfElements = itens[0];
+    if(numberOfElements == 0)
+      break;
     cout<<endl;
-    cin>>numberOfElements;
+    itens.erase(itens.begin());
   }
 }
