d72b0c039d109e4c89078afe7f6c56f1d04b4078
Arthur Câmara <camara.arthur@gmail.com>
2015-03-30 21:44:27 -0300

    IMPEDIDO.cpp done

--- 2-Basic Data Structures/impedido.cpp
+++ 2-Basic Data Structures/impedido.cpp
@@ -3,13 +3,14 @@
 #include <sstream>
 #include <algorithm>
 #include <vector>
+#include <fstream>
 
 /*
-Given the positions for strikers and deffensers 
-in a soccer game, tell if any striker is 
-prevented. (Meaning that the striker is beyond 
-the second-last defender)
-*/
+ Given the positions for strikers and deffensers
+ in a soccer game, tell if any striker is
+ prevented. (Meaning that the striker is beyond
+ the second-last defender)
+ */
 
 using namespace std;
 
@@ -24,5 +25,40 @@ vector<unsigned> parseLine(string line){
 
 
 int main(int argc, char *argv[]) {
+//  ifstream arq(getenv("MYARQ"));
+//  cin.rdbuf(arq.rdbuf());
   
+  string in;
+  getline(std::cin, in);
+  vector<unsigned> strikersAndDefenders = parseLine(in);
+  bool flag = false;
+  while(1) {
+    if(strikersAndDefenders[1]==1){
+      cout<<"Y"<<endl;
+      continue;
+    }
+    flag = false;
+    getline(std::cin, in);
+    vector<unsigned> strikers = parseLine(in);
+    getline(std::cin, in);
+    vector<unsigned> defensers = parseLine(in);
+    sort(defensers.begin(), defensers.end());
+    //Search for a striker after the second last defender
+    unsigned secondLastPosition = *(defensers.begin()+1);
+    for(vector<unsigned>::iterator it = strikers.begin(); it != strikers.end(); ++it) {
+      if(*it < secondLastPosition) {
+        flag = true;
+        break;
+      }
+    }
+    if(flag)
+      cout<<"Y"<<endl;
+    else
+      cout<<"N"<<endl;
+    getline(std::cin, in);
+    vector<unsigned> strikersAndDefenders = parseLine(in);
+    if (strikersAndDefenders[0] == 0) {
+      break;
+    }
+  }
 }
